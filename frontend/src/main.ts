41bbebfe5f6e9787a293b1f22681457a76dda444
Elixandre <elixandrefw77@gmail.com>
2026-09-10 21:13:42 -0300

    fix(frontend): registrar componentes do Vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,15 @@ import { createPinia } from 'pinia'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const vuetify = createVuetify({
+  components,
+  directives,
   theme: {
     defaultTheme: 'barbearia',
     themes: {
